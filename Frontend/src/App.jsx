e146283f8a7c77e0dbeee6e2a39a8532ba56e408
nalin <rmnsbandara@std.appsc.sab.ac.lk>
2024-10-16 12:06:10 +0530

    Fotter aded

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./Components/Header"
 import './App.css'
+import Fotter from "./Components/Fotter"
 
 function App() {
   return (
     <>
       <Header/>
+      <Fotter/>
     </>
   )
 }
